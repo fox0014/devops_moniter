88b32389a8b5ba6f40f03d28328e0eae19f06816
fanfanfan <284130129@qq.com>
2018-10-25 15:25:16 +0800

    celery update

--- aliyun/routing_inspection/tasks.py
+++ aliyun/routing_inspection/tasks.py
@@ -3,12 +3,17 @@
 
 
 from celery import Celery
+from inspection_client_Regions import Regionsmain
 
-app = Celery('tasks', broker='amqp://admin:admin@10.68.60.111:5673/devops',backend='redis://admin@10.68.60.111:26379/1')
+app = Celery('tasks', broker='amqp://admin:admin@10.68.60.111:5673/devops',backend='redis://:admin@10.68.60.111:26379/1')
 
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def aliyuntaskRegionsmain(accessKey,accessSecret,region):
+    return Regionsmain(accessKey,accessSecret,region)
 
 if __name__ == '__main__':
     result = add.delay(30, 42)
